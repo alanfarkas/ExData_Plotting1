42716f5711866fb91f57b179b64b26cb16c43f22
Alan Farkas <afarkas@alvarezandmarsal.com>
2015-06-07 00:30:10 -0500

    Fixed logical operator issue

--- plot1.R
+++ plot1.R
@@ -2,17 +2,18 @@
 # Read power consumption data into data frame, converting the date and time fields
 # into their corrsponding classes
 #
-maxLines = 100
-power_dat <- read.csv("~/ExData_Plotting1/household_power_consumption.txt", sep=";", 
-                        stringsAsFactors = FALSE,
-                        colClasses = c("Date", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
-                      #  ,nrows = maxLines
+maxLines = 1000
+power_dat0 <- read.csv("~/ExData_Plotting1/household_power_consumption.txt", sep=";", 
+                        stringsAsFactors = FALSE, na.strings = c("?"),
+                        colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+#                        ,nrows = maxLines
                         )
 
 #
 # Filter data frame down to just data for the dates 2007-02-01 and 2007-02-02
 #
-power_dat = power_dat[power_dat$Date == "16/12/2006" || power_dat$Date == "01/02/2007" || power_dat$Date == "02/02/2007"]
+power_dat <- power_dat0[power_dat0$Date == "1/2/2007" | power_dat0$Date == "2/2/2007",]
+#power_dat <- power_dat[power_dat$Date == "16/12/2006" || power_dat$Date == "1/2/2007" || power_dat$Date == "2/2/2007",]
 
 
 
