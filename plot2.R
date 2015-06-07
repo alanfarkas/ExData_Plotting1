ec610fa1883fe55b34d8e38a5e46bfd5f78d1941
Alan Farkas <afarkas@alvarezandmarsal.com>
2015-06-07 02:03:21 -0500

    Plot 2 final updates

--- plot2.R
+++ plot2.R
@@ -3,13 +3,10 @@
 # into character fields. It is assumed that the input is located in the same directory
 # as this script.
 #
-maxLines = 1000
-#power_dat <- read.csv("~/ExData_Plotting1/household_power_consumption.txt", sep=";", 
-#                        stringsAsFactors = FALSE, na.strings = c("?"),
-#                        colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
-#                        ,nrows = maxLines
-#                        )
-power_dat <- power_dat1
+power_dat <- read.csv("~/ExData_Plotting1/household_power_consumption.txt", sep=";", 
+                        stringsAsFactors = FALSE, na.strings = c("?"),
+                        colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+                        )
 
 #
 # Filter data frame down to just data for the dates 2007-02-01 and 2007-02-02
@@ -20,9 +17,6 @@ power_dat <- power_dat[power_dat$Date == "1/2/2007" | power_dat$Date == "2/2/200
 #
 # Add a "Date.Time" comlumn for the combines the date and time columns
 #
-#Day <- weekdays(strptime(paste(power_dat$Date, power_dat$Time), format = "%d/%m/%Y %H:%M:%S"),
-#                      abbreviate = TRUE)
-#power_dat <- cbind(Day, power_dat)
 Date.Time <- strptime(paste(power_dat$Date, power_dat$Time), format = "%d/%m/%Y %H:%M:%S")
 power_dat <- cbind(Date.Time, power_dat)
 
