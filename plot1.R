81514a8bbe64e7a185a4c98075ee91dd2939cb5c
Alan Farkas <afarkas@alvarezandmarsal.com>
2015-06-07 00:31:46 -0500

    Plot 1 code finalized

--- plot1.R
+++ plot1.R
@@ -3,7 +3,7 @@
 # into their corrsponding classes
 #
 maxLines = 1000
-power_dat0 <- read.csv("~/ExData_Plotting1/household_power_consumption.txt", sep=";", 
+power_dat <- read.csv("~/ExData_Plotting1/household_power_consumption.txt", sep=";", 
                         stringsAsFactors = FALSE, na.strings = c("?"),
                         colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
 #                        ,nrows = maxLines
@@ -12,9 +12,8 @@ power_dat0 <- read.csv("~/ExData_Plotting1/household_power_consumption.txt", sep
 #
 # Filter data frame down to just data for the dates 2007-02-01 and 2007-02-02
 #
-power_dat <- power_dat0[power_dat0$Date == "1/2/2007" | power_dat0$Date == "2/2/2007",]
+power_dat <- power_dat0[power_dat$Date == "1/2/2007" | power_dat$Date == "2/2/2007",]
 #power_dat <- power_dat[power_dat$Date == "16/12/2006" || power_dat$Date == "1/2/2007" || power_dat$Date == "2/2/2007",]
-
 
 
 
