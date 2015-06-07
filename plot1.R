eed8eae176775b150244f409226ee1a23858b529
Alan Farkas <afarkas@alvarezandmarsal.com>
2015-06-06 23:20:29 -0500

    First cut a plot 1

---
+++ plot1.R
@@ -0,0 +1,28 @@
+# 
+# Read power consumption data into data frame, converting the date and time fields
+# into their corrsponding classes
+#
+maxLines = 100
+power_dat <- read.csv("~/ExData_Plotting1/household_power_consumption.txt", sep=";", 
+                        stringsAsFactors = FALSE,
+                        nrows = maxLines)
+
+#
+# Filter data frame down to just data for the dates 2007-02-01 and 2007-02-02
+#
+power_dat = power_dat[power_dat$Date == "16/12/2006" || power_dat$Date == "01/02/2007" || power_dat$Date == "02/02/2007"]
+
+
+
+
+#
+# Draw histogram
+#
+hist(power_dat$Global_active_power, main = "Gobal Active Power", col = "red", 
+        xlab = "Global Acitve Power (kilowatts)")
+
+#
+# Save histogram to png
+#
+dev.copy(png, filename = "plot1.png")
+dev.off()
